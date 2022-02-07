50e7d4b7855d2c22375804e4ed6df51980cb1939
opadam <54212616+opaDam@users.noreply.github.com>
2022-02-07 13:00:42 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const list = document.querySelector('.list');
+const li = document.querySelectorAll("li");
+
+const audio = new Audio();
+console.log(js_data_array);
+let song_name, song_path, temp_toggle;
+
+// select song
+    list.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(e);
+        if (temp_toggle) { temp_toggle.classList.remove('playing') }
+        e.target.classList.add('playing');
+        temp_toggle = e.target;
+        song_name = e.target.innerText;
+        playsong();
+    })
+
+let playsong = () => {
+    song_path = `./audio/${song_name}.mp3`;
+    audio.src = song_path;
+    audio.play();
+    
+}
+
+
+
+
+
